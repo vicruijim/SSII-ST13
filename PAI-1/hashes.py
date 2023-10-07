c3d97c78a82a967af37c52e4acc77f71a3b63b5a
andferrod <andferrod5@alum.us.es>
2023-10-07 17:02:31 +0200

    MD5 a SHA256

--- PAI-1/hashes.py
+++ PAI-1/hashes.py
@@ -4,11 +4,11 @@ import sqlite3
 
 def calcula_hash(fichero):
     try:
-        hash_md5 = hashlib.md5()
+        hash_sha256 = hashlib.sha256()
         with open(fichero, "rb") as archivo:
             for bloque in iter(lambda: archivo.read(4096), b""): #b"" condicion para que finalice el bucle al encontrarse una cadena de bytes vacia 
-                hash_md5.update(bloque)
-        return hash_md5.hexdigest()
+                hash_sha256.update(bloque)
+        return hash_sha256.hexdigest()
     except FileNotFoundError:
         return "El archivo no se encontró."
     except Exception as e:
