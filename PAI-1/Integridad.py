5a1a6b49a9ab08a0cc6c94dad43e1673d2742667
vicruijim <ruiz.victor.0493@gmail.com>
2023-10-08 17:13:39 +0200

    Añadida comprobacion diaria y deteccion de ficheros eliminados

--- PAI-1/Integridad.py
+++ PAI-1/Integridad.py
@@ -3,13 +3,26 @@ from datetime import datetime
 import schedule
 from datetime import datetime
 import time
+
 def ver_hashes(dir):
+    list_ficheros = os.listdir(dir)
     cambio = False
     conn = sqlite3.connect("hashes.db")
     conn.text_factory=str
-    for fichero in os.listdir(dir):
+    cursor = conn.execute("SELECT * FROM HASHES")
+    serranito = cursor.fetchall()
+    if len(list_ficheros) < len(serranito):
+         for alioli in serranito:
+              if alioli[0] not in list_ficheros:
+                     cambio = True
+                     print(f"El archivo {alioli[0]}")
+                     with open("./Logs/Logs.txt", "a") as archivo:
+                        hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                        archivo.write(f"El archivo {alioli[0]} ha sido borrado {hora}\n")   
+                    
+    for fichero in list_ficheros:
         hash = calcula_hash("./"+dir+"/"+fichero)
-        cursor = conn.execute("SELECT * FROM HASHES WHERE HASH = ?",(hash,))
+        cursor = conn.execute("SELECT NOMBRE FROM HASHES WHERE HASH = ?",(hash,))
         a = cursor.fetchone()
         if a == None:
             cambio = True
@@ -28,13 +41,15 @@ def ver_hashes(dir):
 def comprobacion_diaria():
     # Programa la comprobación diaria para ejecutarse todos los días a una hora específica
     #schedule.every().day.at("02:00").do(ver_hashes, "./integridad") #frecuencia diaria
-    schedule.every(5).seconds.do(ver_hashes, "./integridad")
+    schedule.every(5).seconds.do(ver_hashes, "./integridad")#prueba frecuencia cada 5 s
     # Ejecuta la programación
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 # Ejecuta la función para programar la comprobación diaria
+def informe_mensual():
+     pass
+
 if __name__ == "__main__":
     comprobacion_diaria()
-   
--- PAI-1/hashes.py
+++ PAI-1/hashes.py
@@ -26,7 +26,7 @@ def almacenar_fichero(dir):
         hash = calcula_hash("./"+dir+"/"+fichero)
         cursor = conn.execute("INSERT INTO HASHES (NOMBRE,HASH) VALUES (?,?)",(fichero,hash))
     conn.commit()
-    cursor = conn.execute("SELECT * FROM HASHES")
+    cursor = conn.execute("SELECT NOMBRE FROM HASHES")
     print(cursor.fetchall())
     conn.close()
 
