6a5229df31e14e86fd6d0f46b62afa502fd71464
vicruijim <ruiz.victor.0493@gmail.com>
2023-10-14 20:00:31 +0200

    sigue con el cliente Andres

--- PAI-2/cliente.py
+++ PAI-2/cliente.py
@@ -1,13 +1,34 @@
-# clientsocket.py
-
+import hmac
+import hashlib
+import os
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 3030  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+def calcular_hmac(mensaje,clave,nonce):
+     
 
-print(f"Received {data!r}")
+def crear_mensaje(mensaje):
+     nonce = os.urandom(16) #Calculamos el nonce 
+     h = hmac.new(key=key.encode(), mensaje.encode('utf-8'), hashlib.sha256) #calculamos el mac
+     mac = h.hexdigest()#
+     enviar = 
+
+
+
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    
+    client_socket.connect((HOST, PORT))
+
+    mensaje = "Este es un mensaje de prueba."
+    nonce = generar_nonce()
+    hmac_calculado = calcular_hmac(mensaje, nonce)
+
+    mensaje_con_hmac = f"{mensaje}|{hmac_calculado}|{nonce.hex()}"
+
+    client_socket.sendall(mensaje_con_hmac.encode())
+
+    print("Mensaje enviado al servidor.")
--- PAI-2/servidor.py
+++ PAI-2/servidor.py
@@ -3,10 +3,14 @@ import hmac
 import hashlib
 import socket
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 3030  # Port to listen on (non-privileged ports are > 1023)
+nonces_utilizados = set()
 
-def calcular_hmac(mensahe,clave,nonce):
-     h = hmac.new(clave_secreta, mensaje.encode('utf-8'), hashlib.sha256)
+def calcular_hmac(mensaje,clave,nonce):
+     h = hmac.new(clave, mensaje.encode('utf-8'), hashlib.sha256)
      return h.hexdigest()
 
+
+
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 3030  # Port to listen on (non-privileged ports are > 1023)
