77c132c66841da6441a14c8de7595de5aa21b059
andferrod <andferrod5@alum.us.es>
2023-10-15 18:53:43 +0200

    cambios serverpy calcular mensaje y errores al recibirlo

--- PAI-2/servidor.py
+++ PAI-2/servidor.py
@@ -1,16 +1,80 @@
 # serversocket.py
+
+import socket
 import hmac
 import hashlib
-import socket
-
-nonces_utilizados = set()
-
-def calcular_hmac(mensaje,clave,nonce):
-     h = hmac.new(clave, mensaje.encode('utf-8'), hashlib.sha256)
-     return h.hexdigest()
-
-
-
+import datetime
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 3030  # Port to listen on (non-privileged ports are > 1023)
+
+mensajesIntegros = 0
+totalmensajes = 0
+
+def calcular_hmac(mensaje,clave,nonce):
+     if nonceUnico(nonce):
+          h = hmac.new(clave, mensaje.encode('utf-8'), hashlib.sha256)
+          return h.hexdigest()
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+        if(data):
+            partes = data.split("|")
+
+            mensaje = partes[0].strip()  
+            nonce = partes[1].strip() 
+            mac = partes[2].strip() 
+
+            clave = 123
+
+            macMensajeCalculado = calcular_hmac(mensaje, clave,  nonce)
+
+
+            if (macMensajeCalculado == mac):
+                    mensajesIntegros = mensajesIntegros + 1
+                    
+            else:
+                    print("Error: Claves no coincidentes. Apuntando en el log de errores...")
+
+                    with open("./logs/errors.log", "a") as errores:
+                        now = datetime.now()
+                        currentTime = now.strftime("%d/%m/%Y %H:%M:%S")
+                        errores.write(f"Posible ataque Man-in-the-middle a las {currentTime}. Mensaje afectado:{data}\n")
+                    
+                    
+                
+            totalmensajes = totalmensajes + 1
+
+            
+        print("Conexión cerrada")
+
+
+
+
+def nonceUnico(nonce):
+    with open("./nonces.txt", "rb") as nonces:
+        for line in nonces:
+            if (line.strip().decode() == nonce):
+                print("Error: Nonce ya utilizado. Apuntando en log de errores...")
+
+                with open("./logs/errors.log", "a") as errores:
+                    now = datetime.now()
+                    currentTime = now.strftime("%d/%m/%Y %H:%M:%S")
+                    errores.write(f" Detectado ataque reply a las {currentTime}, nonce ya registrado {nonce} \n")
+
+                return False
+
+    with open("./nonces.txt", "a") as writeNonces:
+        writeNonces.write(nonce + "\n")
+
+    return True
