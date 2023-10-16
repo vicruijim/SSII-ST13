e6c7541bb5cd5986abcb78a57289ba361bcdcfb9
vicruijim <ruiz.victor.0493@gmail.com>
2023-10-16 15:12:19 +0200

    cambio calculo del nonce

--- PAI-2/cliente.py
+++ PAI-2/cliente.py
@@ -1,6 +1,6 @@
 import hmac
 import hashlib
-import os
+import secrets
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
@@ -9,10 +9,12 @@ PORT = 3030  # The port used by the server
 key = "123"
 
 def crear_mensaje(m,clave):
-     nonce = os.urandom(16) #Calculamos el nonce 
+     #nonce = os.urandom(16) #Calculamos el nonce
+     nonce = secrets.token_hex(16) 
      h = hmac.new(key=clave.encode(), msg=m.encode('utf-8'), digestmod=hashlib.sha256) #calculamos el mac
      mac = h.hexdigest()#
-     mensaje = m + "|" + mac + "|" + nonce  
+     print(nonce)
+     mensaje = str(m) + "|" + str(mac) + "|" + str(nonce)  
      return mensaje
 
 
@@ -26,8 +28,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         message = input("Escribe un mensaje (o 'exit' para salir): ")
         if message == 'exit':
             break
-        crear_mensaje(message,clave= key)
-        s.sendall(message.encode())
+        cagao_de_sueño=crear_mensaje(message,clave= key)
+        s.sendall(cagao_de_sueño.encode())
         data = s.recv(1024)
         print(f"Received {data!r}")
 
