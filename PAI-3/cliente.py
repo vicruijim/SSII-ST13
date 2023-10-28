af1233f6819bf2c518961ba4f5cbf88e23bd0ec9
vicruijim <ruiz.victor.0493@gmail.com>
2023-10-28 19:45:35 +0200

    ya se conectan, falta crear multiples conexiones y funcionalidad cliente servidor del ejercicio

--- PAI-3/cliente.py
+++ PAI-3/cliente.py
@@ -1,28 +1,18 @@
+
 import socket
 import ssl
 
+HOST = "localhost"  # The server's hostname or IP address
+PORT = 3030  # The port used by the server
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 3030  # Port to listen on (non-privileged ports are > 1023)
-
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+ssl_context.minimum_version = ssl.TLSVersion.TLSv1_3
+ssl_context.load_verify_locations(cafile='./certificado/servidor.crt')
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    (client_socket, (client_ip, client_port)) = s.accept()
-    print ("Connection accepted from %s:%s. Processing the request..." % (client_ip, client_port))
-    conn = ssl.wrap_socket(client_socket, server_side=True, 
-        certfile="./certificates/server.crt", keyfile="./certificates/server.key",
-        ssl_version=ssl.PROTOCOL_TLSv1_3)
-    print("Versión TLS utilizada:", conn.version())
+    s.connect((HOST, PORT))
 
-
-
-
-
-
-"""
-main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-main_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2
-"""
+    with ssl_context.wrap_socket(s, server_hostname=HOST) as conn:
+        conn.send(b'Hola, servidor seguro con TLS 1.3\n')
+        data = conn.recv(1024)
+        print('Respuesta del servidor:', data.decode())
+        conn.close()
---
+++ PAI-3/servidor.py
@@ -0,0 +1,25 @@
+import socket
+import ssl
+
+
+HOST = "localhost"  # Standard loopback interface address (localhost)
+PORT = 3030  # Port to listen on (non-privileged ports are > 1023)
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(certfile='./certificado/servidor.crt', keyfile='./certificado/servidor.key')
+ssl_context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        conn = ssl_context.wrap_socket(conn, server_side=True)  # Aplicar SSL a la conexión
+        with conn:
+            print('Conexión aceptada de:', addr)
+
+            # Trabaja con la conexión como lo necesites
+            conn.send(b'Bienvenido al servidor seguro con TLS 1.3\n')
+            data = conn.recv(1024)
+            print('Datos recibidos:', data.decode())
+
