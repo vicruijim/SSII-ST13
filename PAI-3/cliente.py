8c84f24450ad639839ffa26d970faeb7341399cf
vicruijim <ruiz.victor.0493@gmail.com>
2023-10-31 11:12:01 +0100

    Ya hay login chavales

--- PAI-3/cliente.py
+++ PAI-3/cliente.py
@@ -2,23 +2,25 @@
 import socket
 import ssl
 
-#HOST = "localhost"  # The server's hostname or IP address
+HOST = "localhost"  # The server's hostname or IP address
 PORT = 3031  # The port used by the server
-HOST= "DESKTOP-NE5LGKC"
+#HOST= "DESKTOP-NE5LGKC"
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 ssl_context.minimum_version = ssl.TLSVersion.TLSv1_3
-ssl_context.load_verify_locations(cafile='./cert_remoto/servidor.crt')
+#ssl_context.load_verify_locations(cafile='./cert_remoto/servidor.crt')
+ssl_context.load_verify_locations(cafile='./certificado/servidor.crt')
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
     with ssl_context.wrap_socket(s, server_hostname=HOST) as conn:
+       """
         conn.send(b'Hola, servidor seguro con TLS 1.3\n')
         data = conn.recv(1024)
         print('Respuesta del servidor:', data.decode())
-
-        user = input("Usuario:")
-        password = input("Contraseña:")
-        msj = input("Mensaje:")
-        datos = (user + "|" + password + "|"+ msj)
-        conn.send(datos.encode)
+        """
+       user = input("Usuario:")
+       password = input("Contraseña:")
+       msj = input("Mensaje:")
+       datos = (user + "|" + password + "|"+ msj)
+       conn.send(datos.encode())
--- PAI-3/servidor.py
+++ PAI-3/servidor.py
@@ -2,16 +2,17 @@ import socket
 import ssl
 import hashlib
 import sqlite3
-#HOST = "localhost"  # Standard loopback interface address (localhost)
+HOST = "localhost"  # Standard loopback interface address (localhost)
 PORT = 3031  # Port to listen on (non-privileged ports are > 1023)
-HOST= "0.0.0.0"
+#HOST= "0.0.0.0"
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain(certfile='./cert_remoto/servidor.crt', keyfile='./cert_remoto/servidor.key')
+#ssl_context.load_cert_chain(certfile='./cert_remoto/servidor.crt', keyfile='./cert_remoto/servidor.key')
+ssl_context.load_cert_chain(certfile='./certificado/servidor.crt', keyfile='./certificado/servidor.key')
 ssl_context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_TLSv1_2
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
+    bd = sqlite3.connect('users.db')
     s.bind((HOST, PORT))
     s.listen()
     while True:
@@ -23,12 +24,24 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
              # Trabaja con la conexión como lo necesites
             # conn.send(b'Bienvenido al servidor seguro con TLS 1.3\n')
             data = conn.recv(1024)
-            # print('Datos recibidos:', data.decode())
-            #recibir usuario y contraseña 
-            user, contraseña, msj = data.decode.split("|")
-            #consulta en sqlite
+            print('Datos recibidos:', data.decode())
 
+            #recibir usuario y contraseña 
+            user, contraseña, msj = data.decode().split("|")
+            print(user,contraseña,msj)
+            #consulta en sqlite
             hash_sha256 = hashlib.sha256()
-            hash_sha256.update(contraseña)
+            hash_sha256.update(contraseña.encode('UTF-8'))
             hash = hash_sha256.hexdigest()
+            print("el hash es: ", hash)
             #consulta(user hash)
+            cursor = bd.execute("SELECT * FROM USUARIOS WHERE PASSWORD = ? AND NOMBRE = ?",(hash,user))
+            result = cursor.fetchone()
+          
+            cursor.close()
+            if hash == result[1]:
+                print("El mensaje es: ", msj)
+
+            else: 
+                print("Usuario o contraseña incorrectos")
+
