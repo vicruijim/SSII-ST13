ba63a77778ba56ad03a9e9ba09c95a2d777c95ea
vicruijim <ruiz.victor.0493@gmail.com>
2023-10-31 11:44:44 +0100

    probar y modificar son300

---
+++ PAI-3/son300.py
@@ -0,0 +1,25 @@
+import threading
+import socket
+import ssl
+
+HOST = "localhost"
+hilos = 300
+mensaje = "Andres|pajaro|somos 300"
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+ssl_context.minimum_version = ssl.TLSVersion.TLSv1_3
+#ssl_context.load_verify_locations(cafile='./cert_remoto/servidor.crt')
+ssl_context.load_verify_locations(cafile='./certificado/servidor.crt')
+
+def enviarMensaje():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        with ssl_context.wrap_socket(s, server_hostname=HOST) as conn:
+            conn.send(mensaje.encode())
+
+def atacad():
+    
+    for centinela in range(hilos):
+        hilo = threading.Thread(name='Centinela %s' %centinela, 
+                                target=enviarMensaje)
+        hilo.start()
